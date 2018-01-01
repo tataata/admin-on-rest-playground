733cd6daaee766fb183aac96bfa6aee33ca96d03
tataata <fortataata@gmail.com>
2018-01-01 14:52:23 +0100

    display screens with posts and users

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Yo!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import { PostList } from './posts';
+import { UserList } from './users';
+
+const App = () => (
+    <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+        <Resource name="posts" list={PostList} />
+        <Resource name="users" list={UserList} />
+    </Admin>
+);
 
 export default App;
